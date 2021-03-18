8f4a9a97350727e12760ce404ce7e0ed68a39244
Jakub Michalik <Jakubmichalik0000@gmail.com>
2021-03-18 09:28:53 +0100

    PlayWithComputerMode

--- Tic Tac Toe/MainActivity.cs
+++ Tic Tac Toe/MainActivity.cs
@@ -4,16 +4,25 @@ using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using System;
+using System.Threading;
 
 namespace Tic_Tac_Toe
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        enum GameMode
+        {
+            Null = 0,
+            TwoPlayersMode = 1,
+            PlayWithComputer = 2
+        }
+
         #region Controls and varibles
         LinearLayout mainMenuLinearLayout;
         LinearLayout gameLinearLayout;
         Button buttonPlayTwoPlayersMode;
+        Button buttonPlayWithComputer;
 
         Button[] gameButton = new Button[9];
         Button startButton;
@@ -23,7 +32,8 @@ namespace Tic_Tac_Toe
         char player = ' ';
         char[] playerList = new char[2] { 'O', 'X' };
         int freeFields = 9;
-        int gameMode = 0;
+        GameMode gameMode = GameMode.Null;
+        bool IsGameGoOn = false;
         #endregion
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -37,6 +47,7 @@ namespace Tic_Tac_Toe
             mainMenuLinearLayout = FindViewById<LinearLayout>(Resource.Id.linearLayoutMainMenu);
             gameLinearLayout = FindViewById<LinearLayout>(Resource.Id.linearLayoutGame);
             buttonPlayTwoPlayersMode = FindViewById<Button>(Resource.Id.ButtonPlayTwoPlayersMode);
+            buttonPlayWithComputer = FindViewById<Button>(Resource.Id.buttonPlayWithComputer);
 
             gameButton[0] = FindViewById<Button>(Resource.Id.button1);
             gameButton[1] = FindViewById<Button>(Resource.Id.button2);
@@ -54,6 +65,7 @@ namespace Tic_Tac_Toe
 
             // Get events:
             buttonPlayTwoPlayersMode.Click += ButtonPlayTwoPlayersMode_Click;
+            buttonPlayWithComputer.Click += ButtonPlayWithComputer_Click;
 
             gameButton[0].Click += GameButton0_click;
             gameButton[1].Click += GameButton1_click;
@@ -69,16 +81,24 @@ namespace Tic_Tac_Toe
             resetButton.Click += ResetButton_Click;
 
             // After initialize components:
+            IsGameGoOn = false;
             mainMenuLinearLayout.Visibility = Android.Views.ViewStates.Visible;
             gameLinearLayout.Visibility = Android.Views.ViewStates.Gone;
             FieldReset();
+        }
+
+        private void ButtonPlayWithComputer_Click(object sender, EventArgs e)
+        {
+            mainMenuLinearLayout.Visibility = Android.Views.ViewStates.Gone;
+            gameLinearLayout.Visibility = Android.Views.ViewStates.Visible;
+            gameMode = GameMode.PlayWithComputer;
         }
 
         private void ButtonPlayTwoPlayersMode_Click(object sender, EventArgs e)
         {
             mainMenuLinearLayout.Visibility = Android.Views.ViewStates.Gone;
             gameLinearLayout.Visibility = Android.Views.ViewStates.Visible;
-            gameMode = 1;
+            gameMode = GameMode.TwoPlayersMode;
         }
 
         private void GameButton0_click(object sender, EventArgs e)
@@ -128,14 +148,192 @@ namespace Tic_Tac_Toe
 
         private void CheckField(Button argButton)
         {
-            argButton.Enabled = false;
-            argButton.Text = player.ToString();
-            if (freeFields > 0)
+            if (gameMode == GameMode.TwoPlayersMode)
             {
-                freeFields--;
+                argButton.Enabled = false;
+                argButton.Text = player.ToString();
+                if (freeFields > 0)
+                {
+                    freeFields--;
+                }
+                CheckIsEnd();
+                LoadNextPlayer();
             }
-            CheckIsEnd();
-            LoadNextPlayer();
+            else if (gameMode == GameMode.PlayWithComputer && player == playerList[0])
+            {
+                argButton.Enabled = false;
+                argButton.Text = player.ToString();
+                if (freeFields > 0)
+                {
+                    freeFields--;
+                }
+                CheckIsEnd();
+                LoadNextPlayer();
+                if (IsGameGoOn)
+                {
+                    //TODO ruch komputera
+                    LockButtons();
+                    ComputerGo();
+                    CheckIsEnd();
+                    UnlockButtons();
+                    LoadNextPlayer();
+                }
+            }
+        }
+
+        private void UnlockButtons()
+        {
+            foreach (var button in gameButton)
+            {
+                if (button.Text != playerList[0].ToString() && button.Text != playerList[1].ToString())
+                {
+                    button.Enabled = true;
+                }
+            }
+        }
+
+        private void ComputerGo()
+        {
+            Thread.Sleep(10);
+
+            if (IsGameGoOn)
+            {
+                int[] f = new int[9];
+                int i = 0;
+                foreach (var button in gameButton)
+                {
+                    if (button.Text == playerList[0].ToString()) // 1 - gracz
+                    {
+                        f[i] = 1;
+                    }
+                    else if (button.Text == playerList[1].ToString()) // 2 - komputer
+                    {
+                        f[i] = 2;
+                    }
+                    else // 0 - puste
+                    {
+                        f[i] = 0;
+                    }
+                    i++;
+                }
+
+                ComputerEqualsAlghoritm(ref f[0], ref f[4], ref f[8]);
+                ComputerEqualsAlghoritm(ref f[6], ref f[4], ref f[2]);
+
+                ComputerEqualsAlghoritm(ref f[0], ref f[1], ref f[2]);
+                ComputerEqualsAlghoritm(ref f[3], ref f[4], ref f[5]);
+                ComputerEqualsAlghoritm(ref f[6], ref f[7], ref f[8]);
+
+                ComputerEqualsAlghoritm(ref f[0], ref f[3], ref f[6]);
+                ComputerEqualsAlghoritm(ref f[1], ref f[4], ref f[7]);
+                ComputerEqualsAlghoritm(ref f[3], ref f[5], ref f[8]);
+
+                i = 0;
+                foreach (var item in f)
+                {
+                    if (item == 10)
+                    {
+                        gameButton[i].Text = playerList[1].ToString();
+                        if (freeFields > 0)
+                        {
+                            freeFields--;
+                        }
+                        goto END;
+                    }
+                    i++;
+                }
+                i = 0;
+                foreach (var item in f)
+                {
+                    if (item == 20)
+                    {
+                        gameButton[i].Text = playerList[1].ToString();
+                        if (freeFields > 0)
+                        {
+                            freeFields--;
+                        }
+                        goto END;
+                    }
+                    i++;
+                }
+                i = 0;
+                foreach (var item in f)
+                {
+                    if (item == 30)
+                    {
+                        gameButton[i].Text = playerList[1].ToString();
+                        if (freeFields > 0)
+                        {
+                            freeFields--;
+                        }
+                        goto END;
+                    }
+                    i++;
+                }
+            }
+        END:;
+        }
+
+        private void ComputerEqualsAlghoritm(ref int v1, ref int v2, ref int v3)
+        {
+            if (((v1 == 0 || v1 == 2) &&
+                (v2 == 0 || v2 == 2) &&
+                (v3 == 0 || v3 == 2)) &&
+                   ((v1 == 2 && v2 == 2) ||
+                   (v2 == 2 && v3 == 2) ||
+                   (v1 == 2 && v3 == 2)))
+            {
+                if (v1 == 0)
+                {
+                    v1 = 10;
+                }
+                else if (v2 == 0)
+                {
+                    v2 = 10;
+                }
+                else if (v3 == 0)
+                {
+                    v3 = 10;
+                }
+            }
+            else if (v1 == 0 &&
+                     v2 == 1 &&
+                     v3 == 1)
+            {
+                v1 = 20;
+            }
+            else if (v1 == 1 &&
+                     v2 == 0 &&
+                     v3 == 1)
+            {
+                v2 = 20;
+            }
+            else if (v1 == 1 &&
+                     v2 == 1 &&
+                     v3 == 0)
+            {
+                v3 = 20;
+            }
+
+            else if (v1 == 1 &&
+                     v2 == 0 &&
+                     v3 == 0)
+            {
+                v2 = 30;
+            }
+            else if (v1 == 0 &&
+                     v2 == 1 &&
+                     v3 == 0)
+            {
+                v1 = 30;
+            }
+            else if (v1 == 0 &&
+                     v2 == 0 &&
+                     v3 == 1)
+            {
+                v2 = 30;
+            }
+
         }
 
         private void StartButton_Click(object sender, System.EventArgs e)
@@ -148,7 +346,7 @@ namespace Tic_Tac_Toe
             {
                 button.Enabled = true;
             }
-
+            IsGameGoOn = true;
             player = playerList[0];
             showPlayerTextView.Text = player.ToString();
         }
@@ -158,6 +356,7 @@ namespace Tic_Tac_Toe
         {
             resetButton.Visibility = Android.Views.ViewStates.Gone;
             startButton.Visibility = Android.Views.ViewStates.Visible;
+            IsGameGoOn = false;
             FieldReset();
             player = playerList[0];
             showPlayerTextView.Text = player.ToString();
@@ -227,15 +426,17 @@ namespace Tic_Tac_Toe
                     endGameTextView.Text = "Gracz \"" + p + "\" wygrał!";
                     resetButton.Visibility = Android.Views.ViewStates.Gone;
                     startButton.Visibility = Android.Views.ViewStates.Visible;
+                    IsGameGoOn = false;
                     break;
                 }
-                else if (freeFields == 0)
+                else if (freeFields <= 0)
                 {
                     LockButtons();
                     endGameTextView.Visibility = Android.Views.ViewStates.Visible;
                     endGameTextView.Text = "Koniec gry, nikt nie wygrał.";
                     resetButton.Visibility = Android.Views.ViewStates.Gone;
                     startButton.Visibility = Android.Views.ViewStates.Visible;
+                    IsGameGoOn = false;
                 }
             }
         }
